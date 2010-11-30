9fe4383eb74eb5ca5c9bbf2b6e81202d5ac407be
Francis Giraldeau <francis.giraldeau@usherbrooke.ca>
2010-11-30 16:56:52 -0500

    Add test for basic trace loading

--- junit/org/lttng/flightbox/junit/TestTraceReader.java
+++ junit/org/lttng/flightbox/junit/TestTraceReader.java
@@ -1,15 +1,25 @@
 package org.lttng.flightbox.junit;
 
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.eclipse.linuxtools.lttng.jni.JniTrace;
+import org.eclipse.linuxtools.lttng.jni.exception.JniException;
+import org.eclipse.linuxtools.lttng.jni.factory.JniTraceFactory;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestTraceReader {
 	
-	String trace_dir;
+	static String trace_dir;
 	static public String trace_dir_var = "TRACE_DIR";
+	static public String[] trace_names = {"sleep-1x-1sec", "burn-1x-1sec", "burn-8x-1sec"};
 	
 	@BeforeClass
-	public void setUp() {
+	public static void setUp() {
 		trace_dir = System.getenv(trace_dir_var);
 		if (trace_dir == null) {
 			throw new RuntimeException("TRACE_DIR not set");
@@ -17,7 +27,13 @@ public class TestTraceReader {
 	}
 	
 	@Test
-	public void testLoadSimpleTrace() {
-		
+	public void testLoadSimpleTrace() throws JniException {
+		JniTrace trace = JniTraceFactory.getJniTrace(new File(trace_dir, "sleep-1x-1sec").toString());
+		Short[] major = {2};
+		Short[] minor = {3,5,6};
+		ArrayList<Short> major_list = new ArrayList<Short>(Arrays.asList(major));
+		ArrayList<Short> minor_list = new ArrayList<Short>(Arrays.asList(minor));
+		assertTrue(major_list.contains(trace.getLttMajorVersion()));
+		assertTrue(minor_list.contains(trace.getLttMinorVersion()));
 	}
 }
--- src/org/lttng/flightbox/io/TraceReader.java
+++ src/org/lttng/flightbox/io/TraceReader.java
@@ -1,5 +1,24 @@
 package org.lttng.flightbox.io;
 
+import org.eclipse.linuxtools.lttng.jni.JniTrace;
+import org.eclipse.linuxtools.lttng.jni.exception.JniException;
+import org.eclipse.linuxtools.lttng.jni.factory.JniTraceFactory;
+
 public class TraceReader {
 
+	String trace_path;
+	JniTrace trace; 
+	
+	public TraceReader(String trace_path) {
+		this.trace_path = trace_path;
+	}
+	
+	public void loadTrace() throws JniException {
+		trace = JniTraceFactory.getJniTrace(trace_path);
+	}
+	
+	public short getTraceVersion() {
+		return trace.getLttMajorVersion();
+	}
+	
 }
