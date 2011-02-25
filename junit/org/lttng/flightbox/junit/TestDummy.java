5a6c51c60ab36aadd944883eaf79f7ca2b342e2d
Francis Giraldeau <francis.giraldeau@usherbrooke.ca>
2011-02-25 18:31:39 -0500

    Add ant build system and required dependencies
    As of now, a modified version of swtchart is required to run flightbox. We
    added the source here because upstream didn't merged the required patch.
    
    The jar-all target creates an archive with all dependencies resolved into one
    convenient package. The jni trace read library is still needed.
    
    The junit target is broken at this moment.

---
+++ junit/org/lttng/flightbox/junit/TestDummy.java
@@ -0,0 +1,14 @@
+package org.lttng.flightbox.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestDummy {
+
+	@Test
+	public void testDummy() {
+		assertTrue(true);
+	}
+	
+}
