ba6e0789ce40f73d4e5432a3b0e9c8b4f1e47bcf
Francis Giraldeau <francis.giraldeau@usherbrooke.ca>
2011-04-04 16:44:55 -0400

    Adding main for dependency analysis

--- junit/org/lttng/flightbox/junit/model/TestDependencyAnalysis.java
+++ junit/org/lttng/flightbox/junit/model/TestDependencyAnalysis.java
@@ -1,17 +1,20 @@
 package org.lttng.flightbox.junit.model;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.linuxtools.lttng.jni.exception.JniException;
 import org.junit.Test;
-import org.lttng.flightbox.dep.BlockingReport;
+import org.lttng.flightbox.dep.BlockingItem;
 import org.lttng.flightbox.dep.BlockingTaskListener;
-import org.lttng.flightbox.io.TraceEventHandlerModel;
-import org.lttng.flightbox.io.TraceEventHandlerModelMeta;
-import org.lttng.flightbox.io.TraceReader;
+import org.lttng.flightbox.io.ModelBuilder;
 import org.lttng.flightbox.junit.Path;
 import org.lttng.flightbox.model.SystemModel;
 import org.lttng.flightbox.model.Task;
+import org.lttng.flightbox.model.WaitInfo;
 
 public class TestDependencyAnalysis {
 
@@ -22,25 +25,22 @@ public class TestDependencyAnalysis {
 		BlockingTaskListener listener = new BlockingTaskListener();
 		model.addTaskListener(listener);
 
-		// read metadata and statedump
-		TraceEventHandlerModelMeta handlerMeta = new TraceEventHandlerModelMeta();
-		handlerMeta.setModel(model);
-		TraceReader readerMeta = new TraceReader(tracePath);
-		readerMeta.register(handlerMeta);
-		readerMeta.process();
-
-		// read all trace events
-		TraceEventHandlerModel handler = new TraceEventHandlerModel();
-		handler.setModel(model);
-		TraceReader readerTrace = new TraceReader(tracePath);
-		readerTrace.register(handler);
-		readerTrace.process();
+		ModelBuilder.buildFromTrace(tracePath, model);
 
 		// verify there is /bin/sleep task as child of sleep-1x-1sec
-		Task foundTask = model.getLatestTaskByCmdBasename("sleep-1x-1sec");
-		StringBuilder str = new StringBuilder();
-		BlockingReport.printReport(str, listener.getBlockingItems());
-		System.out.println(str.toString());
+		Task foundTask = model.getLatestTaskByCmdBasename("sleep");
+		List<BlockingItem> blockingItems = listener.getBlockingItems();
+		List<BlockingItem> taskItems = new ArrayList<BlockingItem>();
+		for (BlockingItem item: blockingItems) {
+			if (item.getTask() == foundTask) {
+				taskItems.add(item);
+			}
+		}
+
+		assertEquals(1, taskItems.size());
+		WaitInfo info = taskItems.get(0).getWaitInfo();
+		double duration = info.getEndTime() - info.getStartTime();
+		assertEquals(1000000000.0, duration, 10000000.0);
 	}
 
 }
