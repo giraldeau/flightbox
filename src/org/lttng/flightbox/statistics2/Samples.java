a9d14f6633043ac8f2ee3af50e26d85ec7e025b6
Francis Giraldeau <francis.giraldeau@usherbrooke.ca>
2011-06-07 12:53:32 -0400

    Attempt to use generics for samples
    humm... harder than expected. Numbers can't be used as basic type in this case
    because arithmetic is undefined.

---
+++ src/org/lttng/flightbox/statistics2/Samples.java
@@ -0,0 +1,35 @@
+package org.lttng.flightbox.statistics2;
+
+import java.util.ArrayList;
+
+public class Samples<X extends Number,Y extends Number> {
+/*
+	X start;
+	X end;
+	X precision;
+	ArrayList<Y> samples;
+	int initialCapacity;
+	
+	public Samples(X start, X end, X precision) {
+		this.start = start;
+		this.end = end;
+		this.precision = precision;
+	}
+	
+	public Y sum(X x1, X x2) {
+		Y total;
+		int i = sampleIndex(x1);
+		int j = sampleIndex(x2);
+		for (int x=i; x<j; x++) {
+			total += samples.get(x);
+		}
+		return total;
+	}
+	
+	public int sampleIndex(X x) {
+		X duration = end - start;
+		int index = x / duration * samples.size();
+		return index;
+	}
+	*/
+}
