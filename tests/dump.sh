4e8f3726f11b26659a0a3f031be847b3cfcd9874
Francis Giraldeau <francis.giraldeau@gmail.com>
2011-06-28 11:00:37 -0400

    avoid useless output with formatted dump

--- tests/dump.sh
+++ tests/dump.sh
@@ -5,4 +5,4 @@ test -d "$1" || exit 1
 echo "dump $1"
 out=strace/
 mkdir -p $out
-exec lttv -m textDump -t $1 > $out/$(basename $1).dump
+exec lttv -m formattedDump -F "%c.%e %s.%n %p %m" -t $1 > $out/$(basename $1).dump
