27bf65e45b673f338a1e908ff0e8ba93ef0496f5
Francis Giraldeau <francis.giraldeau@usherbrooke.ca>
2011-09-13 09:03:45 -0400

    Add unique id to vertex

--- src/org/lttng/flightbox/graph/ExecVertex.java
+++ src/org/lttng/flightbox/graph/ExecVertex.java
@@ -3,13 +3,29 @@ package org.lttng.flightbox.graph;
 public class ExecVertex {
 
 	private String label;
+	private static int count = 0;
+	private int id;
 
+	public ExecVertex() {
+		this(count++);
+	}
+	public ExecVertex(int id) {
+		setId(id);
+	}
 	public String getLabel() {
 		return this.label;
 	}
 
 	public void setLabel(String label) {
 		this.label = label;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
 	}
 
 }
